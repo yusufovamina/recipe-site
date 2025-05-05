913b9dd74d9af3f6f5bc6afc54a93ec002109a90
yusufovamina <yusufovamina@gmail.com>
2025-05-05 21:28:55 +0400

    upd

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const prisma = new PrismaClient();
 
-export const prisma =
-  globalForPrisma.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+export default prisma;
